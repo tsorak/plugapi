7801f967aa95af7ac5996e2bb9f2dc0c49197262
Karl Osterberg <karost0249@gmail.com>
2023-10-31 16:16:39 +0100

    Fix: implement Jason.Encoder for Todo struct

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,3 +1,9 @@
 defmodule Todo do
   defstruct description: "", done: false
+
+  defimpl Jason.Encoder do
+    def encode(%Todo{description: desc, done: done}, opts) do
+      Jason.Encode.map(%{description: desc, done: done}, opts)
+    end
+  end
 end
