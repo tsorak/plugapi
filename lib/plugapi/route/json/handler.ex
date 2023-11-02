488996142af136734f1631894eb08a0771618ea6
Karl Osterberg <karost0249@gmail.com>
2023-11-02 08:42:16 +0100

    Add json endpoint for getting all todos

--- lib/plugapi/route/json/handler.ex
+++ lib/plugapi/route/json/handler.ex
@@ -7,4 +7,12 @@ defmodule Plugapi.Route.Json do
   get "/" do
     send_resp(conn, 200, "Up - #{Mix.env()}")
   end
+
+  get "/todo" do
+    {:ok, todos} =
+      KV.Registry.get_all()
+      |> Jason.encode()
+
+    send_resp(conn, 200, todos)
+  end
 end
